3f6e0e37bafb5049a2ff4995071e21a9c12c3c14
Chris Narvaez <christian77narvaez@gmail.com>
2026-08-11 21:38:44 -0700

    Add sitemap and robots

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://olga-pmutj-website.vercel.app/sitemap.xml",
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://olga-pmutj-website.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
